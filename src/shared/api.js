da495bab6473a34d451688a2f2dbe4f8a52e7b7a
Nazar <83466233+tipanazar@users.noreply.github.com>
2023-12-01 18:51:11 +0000

    upd

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 
-const telegram_bot_token = "6459758252:AAHDu-GegzsU2084iQTC2cWfFC4QwA4UbLs";
-const chat_id = "713610343";
+const { REACT_APP_TELEGRAM_BOT_TOKEN, REACT_APP_CHAT_ID } = process.env;
 
 export const sendDataApi = async (formData) => {
-  console.log("formData: ", formData);
   const { data } = await axios.post(
-    `https://api.telegram.org/bot${telegram_bot_token}/sendMessage?chat_id=${chat_id}&text=${formData}`,
+    `https://api.telegram.org/bot${REACT_APP_TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${REACT_APP_CHAT_ID}&text=${formData}`,
     formData
   );
-//   console.log("Data: ", data);
   return data;
 };
