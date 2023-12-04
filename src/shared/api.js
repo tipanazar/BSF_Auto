021008087da82605028f842b54ab69aa7286701a
Nazar <83466233+tipanazar@users.noreply.github.com>
2023-12-04 22:02:34 +0000

    2 chats feature + header settings

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 
-const { REACT_APP_TELEGRAM_BOT_TOKEN, REACT_APP_CHAT_ID } = process.env;
+const {
+  REACT_APP_TELEGRAM_BOT_TOKEN,
+  REACT_APP_CHAT_ID_1,
+  REACT_APP_CHAT_ID_2,
+} = process.env;
 
 export const sendDataApi = async (formData) => {
-  const { data } = await axios.post(
-    `https://api.telegram.org/bot${REACT_APP_TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${REACT_APP_CHAT_ID}&text=${formData}`,
-    formData
-  );
+  const data = await Promise.all([
+    axios.post(
+      `https://api.telegram.org/bot${REACT_APP_TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${REACT_APP_CHAT_ID_1}&text=${formData}`,
+      formData
+    ),
+    axios.post(
+      `https://api.telegram.org/bot${REACT_APP_TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${REACT_APP_CHAT_ID_2}&text=${formData}`,
+      formData
+    ),
+  ]);
   return data;
 };
